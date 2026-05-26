5b6e20f529e5857020bfbe361cb40277608d4836
Corwin Marsh <7642548+corwinm@users.noreply.github.com>
2026-05-26 16:09:56 -0700

    Add sitemap and robots setup (#4)
    Co-authored-by: Corwin Marsh <corwinm@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 const defaultSiteUrl = 'https://haphazard.dev';
 const isPreviewContext = process.env.CONTEXT === 'deploy-preview' || process.env.CONTEXT === 'branch-deploy';
@@ -8,4 +9,5 @@ const netlifySiteUrl = isPreviewContext ? process.env.DEPLOY_PRIME_URL : process
 // https://astro.build/config
 export default defineConfig({
   site: netlifySiteUrl || process.env.DEPLOY_PRIME_URL || defaultSiteUrl,
+  integrations: [sitemap()],
 });
